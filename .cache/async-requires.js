967e7ddbc39b4ba5ff7df444558dacb768189b01
zxqlxy <liuxinyun6@yahoo.com>
2019-09-08 17:33:04 -0500

    change

--- .cache/async-requires.js
+++
@@ -1,9 +0,0 @@
-// prefer default export if available
-const preferDefault = m => m && m.default || m
-
-exports.components = {
-  "component---cache-dev-404-page-js": () => import("/Users/lxy/Desktop/Rice/Github/zxqlxy.github.ioo/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-404-js": () => import("/Users/lxy/Desktop/Rice/Github/zxqlxy.github.ioo/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
-  "component---src-pages-index-js": () => import("/Users/lxy/Desktop/Rice/Github/zxqlxy.github.ioo/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
-}
-
